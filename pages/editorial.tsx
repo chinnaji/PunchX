509009ecc250281b3c4972e52b698778d76601b0
Chibuike Nnaji <chibuikennaji306@gmail.com>
2022-05-06 01:06:42 -0800

    added other pages

---
+++ pages/editorial.tsx
@@ -0,0 +1,31 @@
+import type { NextPage } from 'next'
+import axios from 'axios'
+import { GetStaticProps } from 'next'
+import { TAllPosts } from '../interfaces'
+import FullPage from '../components/FullPage'
+
+const Editorial: NextPage<{ data: TAllPosts[] }> = ({ data }) => {
+  return (
+    <main>
+      <FullPage pageData={data} />
+    </main>
+  )
+}
+
+export default Editorial
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  // fetch posts in sport category
+  const data = await fetch(
+    'https://punchng.com/wp-json/wp/v2/posts?categories=22&per_page=100'
+  )
+
+  const response = await data.json()
+
+  return {
+    props: {
+      data: response,
+    },
+    revalidate: 60,
+  }
+}
